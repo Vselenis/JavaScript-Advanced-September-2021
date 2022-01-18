997e5d90ac309c87c0898d3b5a0779f1841fa91a
Vselenis <77938348+Vselenis@users.noreply.github.com>
2022-01-18 22:23:58 +0200

    Initial commit

---
+++ 04. DOM - Exercise/03. Accordion/accordion.js
@@ -0,0 +1,12 @@
+function toggle() {
+    const button = document.querySelector('.button');
+    const textDiv = document.getElementById('extra');
+
+    button.textContent = button.textContent == "More" ? "Less" : "More";
+    textDiv.style.display == 'none' || textDiv.style.display == ' '?
+        textDiv.style.display = 'block' : textDiv.style.display = 'none'
+    
+
+    console.log(button);
+
+}
